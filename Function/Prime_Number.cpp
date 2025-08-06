f1e00deb5bf9f616c017e202257a56c3d137431b
Chandra Prakash Rai <crai60433@gmail.com>
2025-08-07 02:27:20 +0530

    check prime

---
+++ Function/Prime_Number.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+    bool isPrime(int n){
+        if(n<2) return 0;
+        
+        for(int i = 2 ; i<n ; i++)
+        {
+            if(n%i==0)
+            return 0;
+        }
+        return 1;
+
+    }
+
+int main()
+{
+    int n ;
+    cout<<"Enter a number";
+    cin>>n;
+
+   cout<< isPrime(n);
+
+
+}
